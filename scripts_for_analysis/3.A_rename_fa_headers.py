0317f16dabc0699641596dac4974015957cede59
Sydney Birch <31546643+sjb1061@users.noreply.github.com>
2020-10-27 10:46:55 -0400

    Update 3.A_rename_fa_headers.py
    update comments

--- scripts_for_analysis/3.A_rename_fa_headers.py
+++ scripts_for_analysis/3.A_rename_fa_headers.py
@@ -2,6 +2,11 @@
 
 #Dont forget to module load linuxbrew/colsa 
 
+#This script takes in 2 arguments, the fasta file that you want to change the headers in and what you want the header to change to and it writes 
+#a new modified fasta file with the altered headers. The format of the new headers will be: >argument_b_t.# 
+#where the # is the position of the header (the scripts counts the headers) and the t represents transcript. 
+#For the actinula transcriptome it will look like Ec_actinula_t.1 : ./3.A_rename_fa_headers.py -a actinula_total.ORP.fa -b Ec_actinula 
+
 #import modules
 import argparse
 
@@ -11,11 +16,12 @@ parser.add_argument("-a", help="input fasta file")
 parser.add_argument("-b", help="header name")
 
 args = parser.parse_args()
-
+#open the fasta file and the new file you are making which will have -mod.fa at the end of the input fasta file 
 try: 
     with open(args.a, "r") as in_handel: 
         with open("{0}-mod.fa".format(args.a), "w") as out_handel: 
             
+            #count the number of headers - if a header shows up write new header out, if the line is not a header write that line out to the file
             count = 1
             for line in in_handel: 
                 if line.startswith(">"): 
