0148aba6e12fe9c7c551a3973b1f16f70eb56694
Sydney Birch <31546643+sjb1061@users.noreply.github.com>
2020-10-30 08:19:10 -0400

    Update 9.C_prep_sig_DEGs_for_heatmap.py
    update comments

--- scripts_for_analysis/9.C_prep_sig_DEGs_for_heatmap.py
+++ scripts_for_analysis/9.C_prep_sig_DEGs_for_heatmap.py
@@ -1,5 +1,9 @@
 #! /usr/bin/env python3
 
+#This script will take in your handmade summary file of sig DEGs (file that you saved in a text file from 9.B_find_sig_degs_in_geneset.py that has each comparisons output). 
+#It will take all of the sig DEG headers and put them in the format of: "header_1","header_2","header_3"..... so you can import this string into R to make heatmaps. 
+#copy the output in the output file and add it to your summary file.  
+
 #import modules
 import argparse
 import time
@@ -7,21 +11,15 @@ import time
 #create an instance of Argument Parser and add positional argument 
 parser = argparse.ArgumentParser()
 parser.add_argument("-a", help="input file: summary file of found sig DEGs")
-parser.add_argument("-b", help="name of output file")
-#parser.add_argument("-c", help="name of output file")
+parser.add_argument("-b", help="name of output file - name of gene set")
 
 args = parser.parse_args()
-
-#This script will take in a file of sig DEGs (that you saved in a text file from 9_find_sig_degs_in_geneset.py) 
-# and will take all of the sig DEG headers and put them in the format of: "header_1","header_2","header_3".....
-#so you can import this string into R to make heatmaps 
-
 
 t1 = time.time()
 
 	
 tot_list = []
-#part 1 populate list with headers
+#part 1 populate list with headers and open output file to write to 
 with open("{0}-list_of_sig_DEGs.txt".format(args.b), "w") as out_handle:
     with open(args.a, "r") as in_handle:
         for line in in_handle:
