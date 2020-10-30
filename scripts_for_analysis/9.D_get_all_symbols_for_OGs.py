103c1d89cec4bec6a4631abf109a9aed758fa433
Sydney Birch <31546643+sjb1061@users.noreply.github.com>
2020-10-30 08:32:15 -0400

    Update 9.D_get_all_symbols_for_OGs.py
    update comments

--- scripts_for_analysis/9.D_get_all_symbols_for_OGs.py
+++ scripts_for_analysis/9.D_get_all_symbols_for_OGs.py
@@ -1,20 +1,29 @@
 #! /usr/bin/env python3
 
+#This script is the last script to prep for the heat map - it will go through the gene set output file from R in step 8 and will create a 
+#output file that has 2 columns seperated by tabs. The first column will have all the orthogroups and the second will contain all of the 
+#gene symbols associated with that orthogroup. If there are multiple gene symbols to one orthogroup it will add each symbol in the second column
+#and seperate them by a comma. These will ulitmately be used to annotate the heatmap in step 10. 
+#ex:
+#OG0000390	CD36
+#OG0000470	GNAT1, GNAT2, GNAT3
+
 #import modules
 import argparse
 
 #create an instance of Argument Parser and add positional argument 
 parser = argparse.ArgumentParser()
 parser.add_argument("-a", help="input file: gene set from R (file with gene_acc OG symbol Actinula_seqid)")
-parser.add_argument("-b", help="output file name - geneset name")
+parser.add_argument("-b", help="output file name - gene set name")
 
 args = parser.parse_args()
 
+#dict set up:
 #key = OG[2]
 #value = [symbol][3] 
 	
 tot_db = {}
-#part 1 populate dictionary with OGs and symbols
+#part 1 populate dictionary with OGs and symbols and open output file to write to 
 with open("{0}-all_symbols_for_OGs.txt".format(args.b), "w") as out_handle:
     with open(args.a, "r") as in_handle:
         for line in in_handle:
@@ -24,7 +33,7 @@ with open("{0}-all_symbols_for_OGs.txt".format(args.b), "w") as out_handle:
             tot_db.setdefault(sp_line[2].strip(),[]) #set key to OG and make list as value
             tot_db[sp_line[2]].append(sp_line[3].strip()) #add symbol
             
-        #print("db after 1st file: ", tot_db)
+        #print("initial dict after population: ", tot_db)
         #print(len(tot_db))   
 
 #part 2 - unique the dictionary
